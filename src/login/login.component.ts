26f8b6a9973b42e51bc682f4772064e793dee002
Reethika Gouru <rethyka9594@gmail.com>
2023-07-22 23:52:27 -0400

    Initial Commit 2

---
+++ src/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'library-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.less']
+})
+export class LoginComponent {
+  // @ts-ignore
+  loginFormGroup : FormGroup;
+
+  ngOnInit() {
+    this.loginFormGroup = new FormGroup({
+      userName : new FormControl('', [Validators.required]),
+      password : new FormControl()
+    })
+  }
+
+  login() {
+    console.log(this.loginFormGroup.value);
+  }
+}
