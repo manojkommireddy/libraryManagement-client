b3ad979bfa08354c9c846f66a2240c3c28548344
thanmaigouru <gouruthanmai@gmail.com>
2023-07-24 22:21:43 -0400

    I have completed my branch and am now submitting the code for review.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {LoginComponent} from '../login/login.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HomeComponent} from '../home/home.component';
+import {LoginComponent} from '../login/login.component';
 
 @NgModule({
   declarations: [
